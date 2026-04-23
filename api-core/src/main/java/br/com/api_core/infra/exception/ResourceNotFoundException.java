f24f728d798b7e7c4bd5e46091e995440f9e0d67
brunomota7 <bruno7motadev@gmail.com>
2026-04-23 16:56:05 -0300

    fix(exception): correcting the type of parameter received by the exception

--- api-core/src/main/java/br/com/api_core/infra/exception/ResourceNotFoundException.java
+++ api-core/src/main/java/br/com/api_core/infra/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package br.com.api_core.infra.exception;
 import org.springframework.http.HttpStatus;
 
 public class ResourceNotFoundException extends BusinessException {
-    public ResourceNotFoundException(String resource) {
+    public ResourceNotFoundException(String resource, String identifier) {
         super(resource + " not found: " + identifier, HttpStatus.NOT_FOUND);
     }
 }
