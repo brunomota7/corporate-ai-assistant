66cd6d52301267a7446b569152b92228ebec926d
brunomota7 <bruno7motadev@gmail.com>
2026-04-22 21:10:55 -0300

    fix(order): adds new custom methods

--- api-core/src/main/java/br/com/api_core/domain/repository/OrderRepository.java
+++ api-core/src/main/java/br/com/api_core/domain/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package br.com.api_core.domain.repository;
 
 import br.com.api_core.domain.Order;
+import br.com.api_core.domain.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.UUID;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
     List<Order> findByUserId(UUID userId);
+    Order findByIdAndUserId(UUID id, UUID userId);
+    List<Order> findAllByStatus(OrderStatus status);
 }
