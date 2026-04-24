1b8521a08d5609211e53b2b1001f02706a1c1463
brunomota7 <bruno7motadev@gmail.com>
2026-04-23 21:57:44 -0300

    feat(audit log): creates methods for searching audit records

--- api-core/src/main/java/br/com/api_core/domain/repository/AuditLogRepository.java
+++ api-core/src/main/java/br/com/api_core/domain/repository/AuditLogRepository.java
@@ -4,8 +4,12 @@ import br.com.api_core.domain.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
+    List<AuditLog> findByUserId(UUID userId);
+    List<AuditLog> findBySessionId(UUID sessionId);
+    List<AuditLog> findByUserIdAndSessionId(UUID userId, UUID sessionId);
 }
